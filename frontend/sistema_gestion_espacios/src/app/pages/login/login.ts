05fc7f4f87382e3d87c336a2c0fd37b81dcc0ee4
cholobackcod <choloman14@gmail.com>
2026-08-29 19:30:13 -0300

    feat: crear componente de login reactivo con validaciones

--- frontend/sistema_gestion_espacios/src/app/pages/login/login.ts
+++ frontend/sistema_gestion_espacios/src/app/pages/login/login.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css']
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder);
+  loginForm = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  errorMessage = '';
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Por favor, completá los campos correctamente.';
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Intentando iniciar sesión con:', this.loginForm.value);
+    this.errorMessage = '';
+  }
+}
