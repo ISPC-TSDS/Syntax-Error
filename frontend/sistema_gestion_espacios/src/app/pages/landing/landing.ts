62cc32e5c907766cf8d193a2927f63354151c0bb
andresbianchiispc <andresbianchi.ok@gmail.com>
2026-08-30 21:36:44 -0300

    feat: actualizar enlaces de reserva para redirigir a la página de inicio de sesión

--- frontend/sistema_gestion_espacios/src/app/pages/landing/landing.ts
+++ frontend/sistema_gestion_espacios/src/app/pages/landing/landing.ts
@@ -3,7 +3,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
